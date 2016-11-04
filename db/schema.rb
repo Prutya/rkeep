0f9ce20f6862fd7a1a8b9164803cb8eca954a22d
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-11-04 20:35:41 +0200

    Add spendings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104182324) do
+ActiveRecord::Schema.define(version: 20161104183259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 20161104182324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_roles_on_name", unique: true, using: :btree
+  end
+
+  create_table "spendings", force: :cascade do |t|
+    t.string   "name",                                                null: false
+    t.decimal  "total",      precision: 10, scale: 2, default: "0.0", null: false
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
   end
 
   create_table "tables", force: :cascade do |t|
---
+++ spec/factories/spendings.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :spending do
+    name "MyString"
+    total "9.99"
+  end
+end
