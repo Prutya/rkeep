c3e17977be2d4d9111aeaeb8ff7d47d28b85a232
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-11-05 18:39:00 +0200

    Add specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,48 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :phone }
     it { should_not allow_value("").for :phone }
   end
+
+  describe 'methods' do
+    before(:all) do
+      @roles = [ Role.new({ name: :admin }), Role.new({ name: :employee }) ]
+    end
+
+    describe 'admin?' do
+      it 'should return true when has admin role' do
+        subject.roles << @roles[0]
+        expect(subject.admin?).to be_truthy
+      end
+      it 'should return false when has no admin role' do
+        expect(subject.admin?).to be_falsy
+      end
+    end
+
+    describe 'employee?' do
+      it 'should return true when has employee role' do
+        subject.roles << @roles[1]
+        expect(subject.employee?).to be_truthy
+      end
+      it 'should return false when has no employee role' do
+        expect(subject.employee?).to be_falsy
+      end
+    end
+  end
+
+  describe 'abilities' do
+    describe 'rails admin' do
+      before(:each) do
+        @ability = AdminAbility.new(subject)
+      end
+
+      it 'should not be able to access rails admin' do
+        @ability.should_not be_able_to :access, :rails_admin
+      end
+
+      it 'should be able to access rails admin if is admin' do
+        subject.roles << Role.new({ name: :admin })
+        @ability = AdminAbility.new(subject)
+        @ability.should be_able_to :access, :rails_admin
+      end
+    end
+  end
 end
