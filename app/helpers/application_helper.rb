decc4d11503d221b9c0982903d36facaea0bfe64
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-11-05 14:44:18 +0200

    Configure Cancancan for Rails Admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  BASE_TITLE = "rkeep"
+
+  def full_title(page_title = '')
+    return "#{page_title} | #{BASE_TITLE}" unless page_title.empty?
+    BASE_TITLE
+  end
 end
