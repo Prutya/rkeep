8f39ba85fde30abd51a1e5e528d0558798ed6afe
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-11-05 21:11:27 +0200

    Add Toastr support for devise forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,20 @@ module ApplicationHelper
     return "#{page_title} | #{BASE_TITLE}" unless page_title.empty?
     BASE_TITLE
   end
+
+  def toastr_flash
+    return "" if flash.empty?
+
+    toasts = flash.map do |key, value|
+      action = key.to_s.gsub(/alert|error/, 'error').gsub('notice', 'success')
+      "toastr.#{action}(\"#{value}\", \"#{action.capitalize}\");"
+    end
+
+    script = <<-HTML
+    <script>$(document).ready(function(){ #{toasts.join} });</script>
+    HTML
+
+    script.html_safe
+  end
+  
 end
--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,23 +2,17 @@ module DeviseHelper
   def devise_error_messages!
     return "" unless devise_error_messages?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
+    messages = resource.errors.full_messages.map { |msg| "toastr.error(\"#{msg}\", \"Error\");" }
 
-    html = <<-HTML
-    <div id="error_explanation">
-      <h2>#{sentence}</h2>
-      <ul>#{messages}</ul>
-    </div>
+    script = <<-HTML
+    <script>$(document).ready(function(){ #{messages.join} });</script>
     HTML
 
-    html.html_safe
+    script.html_safe
   end
 
   def devise_error_messages?
     !resource.errors.empty?
   end
-
+  
 end
