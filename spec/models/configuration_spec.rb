efb3fa44c29bdffb56b16ef33a88e5fd4f3c2e94
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-11-06 02:50:12 +0200

    Fix tests

--- spec/models/configuration_spec.rb
+++ spec/models/configuration_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Configuration, type: :model do
   describe 'validations' do
     subject { FactoryGirl.build(:configuration) }
-    it { should validate_presence_of :time_opens }
+    it { should validate_presence_of :company_name }
     it { should_not allow_value("").for :company_name }
   end
 end
