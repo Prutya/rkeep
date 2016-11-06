c75ce72ec01385d5d559e410599dcc8bafaccb0a
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-11-06 18:56:06 +0200

    Turn static method into ActiveRecord scope

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -4,14 +4,14 @@ class Bill < ApplicationRecord
 
   validates :people_number, presence: true
 
-  def self.current
+  scope :for_shift, -> (current_time = nil) do
+    current_time ||= Time.zone.now
     config = Configuration.last_set
-    current_time = Time.zone.now
     time_opens_today = Time.new(current_time.year, current_time.month, current_time.day,
                                 config.time_opens.hour, config.time_opens.min, 0, 0)
-    time_from = current_time >= time_opens_today ? time_opens_today : time_opens_today + 1.day
+    time_from = current_time >= time_opens_today ? time_opens_today : time_opens_today - 1.day
     time_to   = current_time >= time_opens_today ? time_opens_today + 1.day : time_opens_today
-    
+
     where({ created_at: time_from..time_to })
   end
 end
