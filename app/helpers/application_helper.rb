b57af7c801c0e86983735d3be080a8dcbaa4b669
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-11-22 19:39:40 +0200

    Read page title from configurations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-  BASE_TITLE = "rkeep"
+  DEFAULT_TITLE = "rkeep"
 
   def full_title(page_title = '')
-    return "#{page_title} | #{BASE_TITLE}" unless page_title.empty?
-    BASE_TITLE
+    title = Configuration.last_set.company_name || DEFAULT_TITLE
+    return "#{page_title} | #{title}" unless page_title.empty?
+    title
   end
 
   def toastr_flash
