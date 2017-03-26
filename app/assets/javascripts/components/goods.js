fd6d999c44e8d9fdf6d28f5ec5bbde6a99a8d114
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2017-03-26 20:42:25 +0300

    Fix es6

--- app/assets/javascripts/components/goods.js
+++ app/assets/javascripts/components/goods.js
@@ -1,222 +1,222 @@
 var GoodsEditor = function(container) {
-  var container = container
-  var list = container.find('.goods-container__list')
-  var popupWrapper = null
-  var popup = null
-  var billId = container.data('bill-id')
-  var billItemTemplate = $('#bill-item-template').text()
-  var billItemPopupTemplate = $('#bill-item-popup-template').text()
-  var spinner = container.find('.goods-container__loader')
-  var goods = null
+  var container = container;
+  var list = container.find('.goods-container__list');
+  var popupWrapper = null;
+  var popup = null;
+  var billId = container.data('bill-id');
+  var billItemTemplate = $('#bill-item-template').text();
+  var billItemPopupTemplate = $('#bill-item-popup-template').text();
+  var spinner = container.find('.goods-container__loader');
+  var goods = null;
 
-  init()
+  init();
 
   return {
     list: list,
     popup: popup,
     popupWrapper: popupWrapper,
     billId: billId
-  }
+  };
 
   function init() {
     if (container.length < 1) {
-      return
+      return;
     }
 
-    initCards()
-    initGoods()
+    initCards();
+    initGoods();
   }
 
   function initPopup() {
-    var renderedPopup = Mustache.render(billItemPopupTemplate, {goods: goods})
-    popupWrapper = $(renderedPopup)
-    popup = popupWrapper.find('.good-popup')
+    var renderedPopup = Mustache.render(billItemPopupTemplate, {goods: goods});
+    popupWrapper = $(renderedPopup);
+    popup = popupWrapper.find('.good-popup');
 
-    container.append(popupWrapper)
+    container.append(popupWrapper);
 
-    var openButton = $('#good-popup-trigger')
-    var closeButton = popup.find('.good-popup__button--close')
-    var addButton = popup.find('.good-popup__footer__add-button')
+    var openButton = $('#good-popup-trigger');
+    var closeButton = popup.find('.good-popup__button--close');
+    var addButton = popup.find('.good-popup__footer__add-button');
 
     closeButton.click(function(e) {
-      e.preventDefault()
-      e.stopPropagation()
-      closePopup()
-    })
+      e.preventDefault();
+      e.stopPropagation();
+      closePopup();
+    });
 
     openButton.click(function(e) {
-      e.preventDefault()
-      e.stopPropagation()
-      openPopup()
-    })
+      e.preventDefault();
+      e.stopPropagation();
+      openPopup();
+    });
 
     popup.click(function(e) {
-      e.preventDefault()
-      e.stopPropagation()
-    })
+      e.preventDefault();
+      e.stopPropagation();
+    });
 
     popupWrapper.click(function(e) {
-      e.preventDefault()
-      e.stopPropagation()
-      closePopup()
-    })
+      e.preventDefault();
+      e.stopPropagation();
+      closePopup();
+    });
 
     addButton.click(function(e) {
-      e.preventDefault()
+      e.preventDefault();
 
-      var selectList = popup.find('select[name="good[id]"]')
-      var numberList = popup.find('input[name="good[quantity]"]')
+      var selectList = popup.find('select[name="good[id]"]');
+      var numberList = popup.find('input[name="good[quantity]"]');
 
-      var goodId = selectList.val()
-      var goodQuantity = parseInt(numberList.val())
+      var goodId = selectList.val();
+      var goodQuantity = parseInt(numberList.val());
 
-      closePopup()
+      closePopup();
 
-      addItem({good_id: goodId, quantity: goodQuantity})
-    })
+      addItem({good_id: goodId, quantity: goodQuantity});
+    });
   }
 
   function initCards() {
     loadBill().success(function(data) {
       data.bill.items.forEach(function(item) {
-        var button_disabled = data.bill.closed_at || data.bill.cancelled_at
-        addCard({item: item, button_disabled: button_disabled})
-      })
-      hideSpinner()
+        var button_disabled = data.bill.closed_at || data.bill.cancelled_at;
+        addCard({item: item, button_disabled: button_disabled});
+      });
+      hideSpinner();
     }).error(function() {
-      toastr.error('Error loading bill items.', 'Error')
-    })
+      toastr.error('Error loading bill items.', 'Error');
+    });
   }
 
   function initGoods() {
     return loadGoods().success(function(data) {
-      goods = data.goods
-      initPopup()
+      goods = data.goods;
+      initPopup();
     }).error(function() {
-      toastr.error('Error loading goods list.', 'Error')
-    })
+      toastr.error('Error loading goods list.', 'Error');
+    });
   }
 
-  function addCard(data, shouldHighlight = false) {
-    var element = findCard(data.item.id)
+  function addCard(data, shouldHighlight) {
+    var element = findCard(data.item.id);
 
     if (element.length > 0) {
-      var numberInput = element.find('input[name="good[quantity]"]')
-      var existingNumber = parseInt(numberInput.val())
+      var numberInput = element.find('input[name="good[quantity]"]');
+      var existingNumber = parseInt(numberInput.val());
 
-      numberInput.val(data.item.quantity)
+      numberInput.val(data.item.quantity);
     } else {
-      var rendered = Mustache.render(billItemTemplate, data)
-      var element = $(rendered)
+      var rendered = Mustache.render(billItemTemplate, data);
+      var element = $(rendered);
 
-      initCard(element)
+      initCard(element);
 
-      list.append(element)
+      list.append(element);
       setTimeout(function() {
-        showCard(element)
-      }, 5) // CSS transition hack
+        showCard(element);
+      }, 5); // CSS transition hack
     }
 
     if (shouldHighlight) {
-      highlightCard(element)
+      highlightCard(element);
     }
   }
 
   function initCard(card) {
-    var closeButton = card.find('.good__button--close')
-    var itemId = card.data('bill-item-id')
+    var closeButton = card.find('.good__button--close');
+    var itemId = card.data('bill-item-id');
 
     closeButton.click(function(e) {
-      e.preventDefault()
-      e.stopPropagation()
+      e.preventDefault();
+      e.stopPropagation();
 
       if (!confirm('Are you sure?')) {
-        return
+        return;
       }
 
       removeItem(itemId).success(function(data) {
-        disableCard(card)
-        updateBillInfo(data.bill_item.bill)
+        disableCard(card);
+        updateBillInfo(data.bill_item.bill);
       }).error(function() {
-        toastr.error('Failed to cancel bill item.', 'Error')
-      })
-    })
+        toastr.error('Failed to cancel bill item.', 'Error');
+      });
+    });
   }
 
   function openPopup() {
-    popupWrapper.removeClass('popup-wrapper--hidden')
-    $(document.body).addClass('noscroll')
+    popupWrapper.removeClass('popup-wrapper--hidden');
+    $(document.body).addClass('noscroll');
   }
 
   function closePopup() {
-    popupWrapper.addClass('popup-wrapper--hidden')
-    $(document.body).removeClass('noscroll')
+    popupWrapper.addClass('popup-wrapper--hidden');
+    $(document.body).removeClass('noscroll');
 
-    popup.find('input[name="good[quantity]"]').val(1) // Set to default value
+    popup.find('input[name="good[quantity]"]').val(1); // Set to default value
   }
 
   function highlightCard(card) {
-    card.addClass('good--highlight')
+    card.addClass('good--highlight');
     setTimeout(function() {
-      card.removeClass('good--highlight')
-    }, 1000)
+      card.removeClass('good--highlight');
+    }, 1000);
   }
 
   function findCard(id) {
     return list.find('.good').filter(function(index) {
-      var item = $(this)
-      var itemId = item.data('bill-item-id')
+      var item = $(this);
+      var itemId = item.data('bill-item-id');
 
-      return itemId === id
+      return itemId === id;
     })
   }
 
   function disableCard(card) {
-    card.addClass('good--disabled')
+    card.addClass('good--disabled');
 
-    return card
+    return card;
   }
 
   function enableCard(card) {
-    card.removeClass('good--disabled')
+    card.removeClass('good--disabled');
 
-    return card
+    return card;
   }
 
   function hideCard(card) {
-    card.addClass('good--hidden')
+    card.addClass('good--hidden');
 
-    return card
+    return card;
   }
 
   function showCard(card) {
-    card.removeClass('good--hidden')
+    card.removeClass('good--hidden');
 
-    return card
+    return card;
   }
 
   function showSpinner() {
-    spinner.removeClass('goods-container__loader--hidden')
+    spinner.removeClass('goods-container__loader--hidden');
   }
 
   function hideSpinner() {
-    spinner.addClass('goods-container__loader--hidden')
+    spinner.addClass('goods-container__loader--hidden');
   }
 
   function updateBillInfo(options) {
-    $('#bill-total').text(options.total)
-    $('#bill-subtotal').text(options.subtotal)
+    $('#bill-total').text(options.total);
+    $('#bill-subtotal').text(options.subtotal);
   }
 
   function loadBill() {
     return $.ajax({
       url: '/api/v1/bills/' + billId,
       method: 'GET'
-    })
+    });
   }
 
   function loadGoods() {
-    return $.ajax({url: '/api/v1/goods', method: 'GET'})
+    return $.ajax({url: '/api/v1/goods', method: 'GET'});
   }
 
   function addItem(item) {
@@ -229,9 +229,9 @@ var GoodsEditor = function(container) {
     }).success(function(data) {
       addCard({
         item: data.bill_item
-      }, true)
+      }, true);
 
-      updateBillInfo(data.bill_item.bill)
+      updateBillInfo(data.bill_item.bill);
     })
   }
 
@@ -239,10 +239,10 @@ var GoodsEditor = function(container) {
     return $.ajax({
       url: '/api/v1/bills/' + billId + '/items/' + itemId,
       method: 'DELETE'
-    })
+    });
   }
 }
 
 $(function() {
-  var goodsEditor = new GoodsEditor($('.goods-container'))
-})
+  var goodsEditor = new GoodsEditor($('.goods-container'));
+});
