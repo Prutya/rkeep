6757f570f06fb2a0ae9e118d94898bd1e4d0fe7c
Anton Prudkohliad <aprudkoh@cisco.com>
2018-01-04 20:13:18 +0200

    [+] Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,47 @@
-Role.create({ name: :admin })    unless Role.exists?(name: :admin)
-Role.create({ name: :employee }) unless Role.exists?(name: :employee)
+role_admin    = Role.find_or_create_by({ name: :admin })
+role_employee = Role.find_or_create_by({ name: :employee })
 
 unless Discount.exists?
-  Discount.create({ value:  0.00 })
-  Discount.create({ value:  5.00 })
-  Discount.create({ value: 10.00 })
-  Discount.create({ value: 20.00 })
+  Discount.create!({ value:  0.00 })
+  Discount.create!({ value:  5.00 })
+  Discount.create!({ value: 10.00 })
+  Discount.create!({ value: 20.00 })
+end
+
+unless User.exists?(email: 'admin@example.com')
+  User.create!({
+    email: 'admin@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: 'Admin',
+    last_name: 'Admin',
+    phone: '+380501488228',
+    roles: [role_admin, role_employee]
+  })
+end
+
+unless User.exists?(email: 'employee@example.com')
+  User.create!({
+    email: 'employee@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: 'Employee',
+    last_name: 'Employee',
+    phone: '+380501488228',
+    roles: [role_employee]
+  })
+end
+
+unless Table.any?
+  Table.create!({ name: '1' })
+  Table.create!({ name: '2' })
+  Table.create!({ name: '3' })
+  Table.create!({ name: '4' })
+  Table.create!({ name: '5' })
+  Table.create!({ name: '6' })
+end
+
+unless Good.any?
+  Good.create!({ name: 'Water Sparkling', price: 20.00  })
+  Good.create!({ name: 'Hookah Premium',  price: 150.00 })
 end
